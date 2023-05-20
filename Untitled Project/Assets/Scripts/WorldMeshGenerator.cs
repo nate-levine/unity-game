5b725c9894aafa016031570309ef991a01613be9
nate-levine <n8levine11@gmail.com>
2023-05-20 12:16:24 -0400

    World Mesh shares vertices
    The world mesh is generated with shared vertices

--- Untitled Project/Assets/Scripts/WorldMeshGenerator.cs
+++ Untitled Project/Assets/Scripts/WorldMeshGenerator.cs
@@ -16,8 +16,7 @@ public class WorldMeshGenerator : MonoBehaviour
     public List<Vector2> UVs = new List<Vector2>();
     public List<Color> colors = new List<Color>();
 
-    private int m_verticesIndex = 0;
-    private float[] m_perlinNoise = new float[361];
+    private float[] m_perlinNoise = new float[180];
 
     void Awake()
     {
@@ -31,10 +30,12 @@ public class WorldMeshGenerator : MonoBehaviour
         {
             triangles[i] = new List<int>();
         }
-        for (int i = 0; i < 360; i++)
-            GeneratePerlinNoise(i * Mathf.PI / 720.0f, i);
-        for (int i = 0; i < 360; i++)
-            CreateTile(i * Mathf.PI / 180.0f, i);
+        for (int i = 0; i < 180; i++)
+            GeneratePerlinNoise(i * Mathf.PI / 360.0f, i);
+        for (int i = 0; i < 180; i++)
+            CreateVertices(i * Mathf.PI / 90.0f, i);
+        for (int i = 0; i < 180; i++)
+            CreateTriangles(i);
     }
 
     public void GeneratePerlinNoise(float theta, int index)
@@ -42,45 +43,25 @@ public class WorldMeshGenerator : MonoBehaviour
         // sample a 1d circle or perlin noise from a 2D plane. rather than sampling a straigh line from a 2D plane, sampling from a circle ensures that the noise loops.
         float noiseValue = Mathf.PerlinNoise(Mathf.Cos(theta) * noiseFrequency, Mathf.Sin(theta) * noiseFrequency);
         m_perlinNoise[index] = noiseValue * noiseAmplitude;
-        if (index == 0)
-            m_perlinNoise[360] = noiseValue * noiseAmplitude;
     }
 
-    public void CreateTile(float theta, int index)
+    public void CreateVertices(float theta, int index)
     {
         List<Vector3> initialVertices = new List<Vector3> {
-            new Vector3(Mathf.Cos(theta - (Mathf.PI/180.0f)), Mathf.Sin(theta - (Mathf.PI / 180.0f)), 0.0f) * innerRadius,
-            new Vector3(Mathf.Cos(theta               ), Mathf.Sin(theta                 ), 0.0f) * innerRadius,
-            new Vector3(Mathf.Cos(theta               ), Mathf.Sin(theta                 ), 0.0f) * (outerRadius + m_perlinNoise[index + 1]),
-            new Vector3(Mathf.Cos(theta - (Mathf.PI/180.0f)), Mathf.Sin(theta - (Mathf.PI / 180.0f)), 0.0f) * (outerRadius + m_perlinNoise[index]),
             new Vector3(0.0f, 0.0f, 0.0f),
-            new Vector3(Mathf.Cos(theta               ), Mathf.Sin(theta                 ), 0.0f) * innerRadius,
-            new Vector3(Mathf.Cos(theta - (Mathf.PI/180.0f)), Mathf.Sin(theta - (Mathf.PI / 180.0f)), 0.0f) * innerRadius,
+            new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0.0f) * innerRadius,
+            new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0.0f) * innerRadius,
+            new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0.0f) * (outerRadius + m_perlinNoise[index]),
         };
         foreach (Vector3 vertex in initialVertices)
         {
             vertices.Add(vertex);
         }
-        triangles[0].Add(m_verticesIndex + 0);
-        triangles[0].Add(m_verticesIndex + 1);
-        triangles[0].Add(m_verticesIndex + 2);
-        triangles[0].Add(m_verticesIndex + 0);
-        triangles[0].Add(m_verticesIndex + 2);
-        triangles[0].Add(m_verticesIndex + 3);
-        m_verticesIndex += 4;
-        for (int i = 0; i < 3; i++)
-        {
-            triangles[1].Add(m_verticesIndex);
-            m_verticesIndex++;
-        }
         List<Vector2> initialUVs = new List<Vector2> {
-            new Vector2(0.5f + (Mathf.Cos(theta - Mathf.PI/180.0f) * scalarUV), 0.5f + (Mathf.Sin(theta - Mathf.PI/180.0f) * scalarUV)),
-            new Vector2(0.5f + (Mathf.Cos(theta) * scalarUV), 0.5f + (Mathf.Sin(theta) * scalarUV)),
-            new Vector2(0.5f + (Mathf.Cos(theta) * (scalarUV + 1.0f)), 0.5f + (Mathf.Sin(theta) * (scalarUV + 1.0f))),
-            new Vector2(0.5f + (Mathf.Cos(theta - Mathf.PI/180.0f) * (scalarUV + 1.0f)), 0.5f + (Mathf.Sin(theta - Mathf.PI/180.0f) * (scalarUV + 1.0f))),
             new Vector2(0.5f, 0.5f),
             new Vector2(0.5f + (Mathf.Cos(theta) * scalarUV), 0.5f + (Mathf.Sin(theta) * scalarUV)),
-            new Vector2(0.5f + (Mathf.Cos(theta - Mathf.PI/180.0f) * scalarUV), 0.5f + (Mathf.Sin(theta - Mathf.PI/180.0f) * scalarUV)),
+            new Vector2(0.5f + (Mathf.Cos(theta) * scalarUV), 0.5f + (Mathf.Sin(theta) * scalarUV)),
+            new Vector2(0.5f + (Mathf.Cos(theta) * (scalarUV + 1.0f)), 0.5f + (Mathf.Sin(theta) * (scalarUV + 1.0f))),
         };
         foreach (Vector2 UV in initialUVs)
         {
@@ -91,13 +72,48 @@ public class WorldMeshGenerator : MonoBehaviour
             new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f),
             new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f),
             new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f),
-            new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f),
-            new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f),
-            new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f),
         };
         foreach (Color color in initialColors)
         {
             colors.Add(color);
+        }
+    }
+
+    public void CreateTriangles(int index)
+    {
+        if (index < 179)
+        {
+            // inner world
+            triangles[1].Add((index * 4) + 0);
+            triangles[1].Add((index * 4) + 4);
+            triangles[1].Add((index * 4) + 1);
+            triangles[1].Add((index * 4) + 1);
+            triangles[1].Add((index * 4) + 4);
+            triangles[1].Add((index * 4) + 5);
+            // outer world
+            triangles[0].Add((index * 4) + 2);
+            triangles[0].Add((index * 4) + 6);
+            triangles[0].Add((index * 4) + 3);
+            triangles[0].Add((index * 4) + 3);
+            triangles[0].Add((index * 4) + 6);
+            triangles[0].Add((index * 4) + 7);
+        }
+        if (index == 179)
+        {
+            // inner world
+            triangles[1].Add((index * 4) + 0);
+            triangles[1].Add((0 * 4) + 0);
+            triangles[1].Add((index * 4) + 1);
+            triangles[1].Add((index * 4) + 1);
+            triangles[1].Add((0 * 4) + 0);
+            triangles[1].Add((0 * 4) + 1);
+            // outer world
+            triangles[0].Add((index * 4) + 2);
+            triangles[0].Add((0 * 4) + 2);
+            triangles[0].Add((index * 4) + 3);
+            triangles[0].Add((index * 4) + 3);
+            triangles[0].Add((0 * 4) + 2);
+            triangles[0].Add((0 * 4) + 3);
         }
     }
 }
